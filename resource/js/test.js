18f1c75619d4b3fbef2e5f3f70ded9fbc534a1bc
nhattybirhanu <55690411+nhattybirhanu@users.noreply.github.com>
2021-09-13 07:43:58 -0500

    module

--- resource/js/test.js
+++ resource/js/test.js
@@ -59,3 +59,58 @@ company.setAge(22);
 company.setSalary(220);
 console.log(company.getSalary());
 console.log(company.increaseSalary(20));
+
+
+var emp = function() {
+
+    var employee = {
+        'nam': 'John',
+        age: 40,
+        salary: 6000,
+
+        getAge: function() { return this.age },
+        getSalary: function() { return this.salary },
+        getName: function() { return this.nam },
+        setAge: function(newAge) { age = newAge; },
+        setSalary: function(newSalary) { salary = newSalary },
+        setName: function(newName) { nam = newName },
+        increaseSalary: function(percentage) { return this.getSalary() + this.getSalary() * percentage },
+        incrementAge: function() { return this.getAge() + 10 }
+    }
+
+    function setName(name) {
+        return employee.setName(name);
+
+    }
+
+    function setSalary() {
+        return employee.setSalary();
+
+    }
+
+    function setAge(age) {
+        return employee.setAge(age);
+
+    }
+
+    function increaseSalary(p) {
+        return employee.increaseSalary(p);
+
+    }
+
+    function incrementAge() {
+        return employee.incrementAge();
+
+    }
+    return {
+        setName: function(n) { return setName(n) },
+        setSalary: function(s) { return setSalary(s) },
+        setAge: function(a) { return setAge(a) },
+        increaseSalary: function(n) { return increaseSalary(n) },
+        incrementAge: function() { return incrementAge() },
+    }
+
+}();
+
+// console.log(emp.increaseSalary(2));
+// console.log(emp.incrementAge());
